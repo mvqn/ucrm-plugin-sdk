b45fede0321b880de6f1eca4465591a3a42f202f
Ryan Spaeth <rspaeth@mvqn.net>
2019-06-26 18:32:17 -0700

    - Created upm.php (UCRM Plugin Manager) - Deleted old composer.php script - Added the upm.php script to the composer binary list. - Fixed bundler to use plugin name instead of project name as the ZIP archive's filename.

--- bin/upm.php
+++ bin/upm.php
@@ -1,6 +1,11 @@
+#!/usr/bin/env php
 <?php
 declare(strict_types=1);
-require __DIR__.'/../vendor/autoload.php';
+
+// Should always be "PLUGIN_ROOT/src" during development.
+$pluginPath = getcwd();
+
+require_once $pluginPath."/vendor/autoload.php";
 
 use UCRM\Common\Plugin;
 
@@ -21,27 +26,32 @@ if($argc === 1)
     die($usage);
 }
 
-$pluginPath = realpath(__DIR__."/../../");
-$pluginName = baseName($pluginPath);
+$projectPath = realpath($pluginPath."/../");
+$pluginName = baseName($projectPath);
 
-if(file_exists(__DIR__."/../../.env"))
-    (new \Dotenv\Dotenv(__DIR__."/../../"))->load();
+if(file_exists($pluginPath."/../.env"))
+    (new \Dotenv\Dotenv($pluginPath."/../"))->load();
 
-Plugin::initialize(__DIR__."/../");
+Plugin::initialize($pluginPath);
 
 // Handle the different command line arguments...
 switch ($argv[1])
 {
     // Perform initialization of the Plugin libraries and create the auto-generated Settings class.
     case "create":
-        Plugin::createSettings("App", "Settings", __DIR__."/../");
+        Plugin::createSettings("App", "Settings", $pluginPath."/src/");
         break;
 
     // Bundle the 'zip/' directory into a package ready for Plugin installation on the UCRM server.
     case "bundle":
-        Plugin::bundle(__DIR__."/../", $pluginName, ".zipignore", __DIR__."/../../");
+        // Create the Settings, if needed!
+        if(!file_exists("$pluginPath/src/App/Settings.php"))
+            Plugin::createSettings("App", "Settings", $pluginPath."/src/");
+
+        Plugin::bundle($pluginPath, \App\Settings::PLUGIN_NAME, ".zipignore", $projectPath);
         break;
 
+    /*
     case "sync":
         $host = getenv("UCRM_SFTP_HOSTNAME");
         $port = 22;
@@ -57,6 +67,7 @@ switch ($argv[1])
             $data = $sftp->download($file);
 
         break;
+    */
 
     // TODO: More commands to come!
 
